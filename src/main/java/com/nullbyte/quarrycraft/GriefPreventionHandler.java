87405c556302953bba1d496a5f6c829dd8ae6eaa
mopsy14 <Redstone.computerboy@gmail.com>
2023-12-26 15:46:25 +0100

    Fixed the lowest y coördinate being checked instead of hardcoded 0. Put everything. Changed everything to work with Maven. Set version to 1.0 Updated libraries to the newest versions.

---
+++ src/main/java/com/nullbyte/quarrycraft/GriefPreventionHandler.java
@@ -0,0 +1,21 @@
+package com.nullbyte.quarrycraft;
+
+import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.ClaimPermission;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.function.Supplier;
+
+public class GriefPreventionHandler {
+	
+	public static boolean checkLocation(Player owner, Location l) {
+		Claim claim = GriefPrevention.instance.dataStore.getClaimAt(l, true, null);
+		if(claim != null){
+			Supplier<String> stringSupplier = claim.checkPermission(owner,ClaimPermission.Build,null);
+			return stringSupplier != null && stringSupplier.get() == null;
+		}
+		return false;
+	}
+}
